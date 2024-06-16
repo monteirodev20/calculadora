407e1cb0ee1b94642f6e46a02bbf20862c030918
António de Sousa <ampseh20@gmail.com>
2024-06-16 02:00:10 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -4,4 +4,24 @@ const calculatorBtnDiv = calculator.querySelector('.calculator__keys');
 calculatorBtnDiv.addEventListener('click', event => {
   button = event.target;
   const { buttonType, key } = button.dataset;
+
+  if (buttonType === 'number') {
+    console.log('Pressed number');
+  }
+
+  if (buttonType === 'decimal') {
+    console.log('Pressed decimal');
+  }
+
+  if (buttonType === 'operator') {
+    console.log('pressed operator');
+  }
+
+  if (buttonType === 'equal') {
+    console.log('pressed equal');
+  }
+
+  if (buttonType === 'clear') {
+    console.log('pressed clear');
+  }
 });
