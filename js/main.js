62e4e2c6eee366f2dcf897e9c5b039196f05b539
António de Sousa <ampseh20@gmail.com>
2024-06-16 18:55:20 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const calculator = document.querySelector('.calculator');
 const calculatorBtnDiv = calculator.querySelector('.calculator__keys');
+const display = calculator.querySelector('.calculator__display');
 
 calculatorBtnDiv.addEventListener('click', event => {
   button = event.target;
   const { buttonType, key } = button.dataset;
+  const result = display.textContent;
 
   if (buttonType === 'number') {
-    console.log('Pressed number');
+    if (result === '0') {
+      display.textContent = key;
+    } else {
+      display.textContent = result + key;
+    }
   }
 
   if (buttonType === 'decimal') {
